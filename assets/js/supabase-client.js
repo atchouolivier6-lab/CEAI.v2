7fc92d9748849ca33b092a1e7eaf19ce00bfb06e
MC <atchouolivier6@gmail.com>
2026-09-15 09:38:17 +0100

    Update supabase-client.js

--- assets/js/supabase-client.js
+++ assets/js/supabase-client.js
@@ -5,7 +5,7 @@
 // =========================================================
 import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 
-const URL_SUPABASE = "https://jdkavcnxfwmodixinvpf.supabase.co/rest/v1/";
+const URL_SUPABASE = "https://jdkavcnxfwmodixinvpf.supabase.co";
 const CLE_ANON_SUPABASE = "sb_publishable_G7ulg-UptUCv2LFFf3NIWA_ip1K3GS0";
 
 export const supabase = createClient(URL_SUPABASE, CLE_ANON_SUPABASE);
